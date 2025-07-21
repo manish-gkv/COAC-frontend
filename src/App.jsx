552d6443a69171806eb6c035210df181b7a078e0
manish-gkv <manishkumarslmp@gmail.com>
2025-07-21 18:24:47 +0530

    added 404 Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import LandingPage from "./components/pages/LandingPage"
 import LoginPage from "./components/pages/Auth"
+import NotFound from "./components/pages/NotFound"
 function App() {
   return (
     <>
@@ -8,6 +9,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/auth/*" element={<LoginPage />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </>
